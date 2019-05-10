2725602d39fe1335310ef8c4ba0d68b02d52b109
Joachim Strömbergson <joachim@assured.se>
2019-05-10 09:12:02 +0200

    Integrated the pblock module into the core. Adding core as build, lint and sim target.

--- src/rtl/poly1305_core.v
+++ src/rtl/poly1305_core.v
@@ -52,24 +52,81 @@ module poly1305_core(
   //----------------------------------------------------------------
   // Registers including update variables and write enable.
   //----------------------------------------------------------------
-  reg [31 : 0] s0_reg;
-  reg [31 : 0] s0_new;
-  reg [31 : 0] s1_reg;
-  reg [31 : 0] s1_new;
-  reg [31 : 0] s2_reg;
-  reg [31 : 0] s2_new;
-  reg [31 : 0] s3_reg;
-  reg [31 : 0] s3_new;
-  reg [31 : 0] s4_reg;
-  reg [31 : 0] s4_new;
-  reg [31 : 0] s5_reg;
-  reg [31 : 0] s5_new;
-  reg          s_we;
+  reg [31 : 0] h0_reg;
+  reg [31 : 0] h0_new;
+  reg [31 : 0] h1_reg;
+  reg [31 : 0] h1_new;
+  reg [31 : 0] h2_reg;
+  reg [31 : 0] h2_new;
+  reg [31 : 0] h3_reg;
+  reg [31 : 0] h3_new;
+  reg [31 : 0] h4_reg;
+  reg [31 : 0] h4_new;
+  reg          h_we;
+
+  reg [31 : 0] c0_reg;
+  reg [31 : 0] c0_new;
+  reg [31 : 0] c1_reg;
+  reg [31 : 0] c1_new;
+  reg [31 : 0] c2_reg;
+  reg [31 : 0] c2_new;
+  reg [31 : 0] c3_reg;
+  reg [31 : 0] c3_new;
+  reg [31 : 0] c4_reg;
+  reg [31 : 0] c4_new;
+  reg          c_we;
+
+  reg [31 : 0] r0_reg;
+  reg [31 : 0] r0_new;
+  reg [31 : 0] r1_reg;
+  reg [31 : 0] r1_new;
+  reg [31 : 0] r2_reg;
+  reg [31 : 0] r2_new;
+  reg [31 : 0] r3_reg;
+  reg [31 : 0] r3_new;
+  reg          r_we;
+
+
+  //----------------------------------------------------------------
+  // Wires.
+  //----------------------------------------------------------------
+  reg pblock_next;
+  wire pblock_ready;
 
 
   //----------------------------------------------------------------
   // Concurrent connectivity for ports etc.
   //----------------------------------------------------------------
+
+
+  //----------------------------------------------------------------
+  // Module instantiations.
+  //----------------------------------------------------------------
+  poly1305_block pblock(
+                        .clk(clk),
+                        .reset_n(reset_n),
+                        .next(pblock_next),
+                        .ready(pblock_ready),
+                        .h0(h0_reg),
+                        .h1(h1_reg),
+                        .h2(h2_reg),
+                        .h3(h3_reg),
+                        .h4(h4_reg),
+                        .c0(c0_reg),
+                        .c1(c1_reg),
+                        .c2(c2_reg),
+                        .c3(c3_reg),
+                        .c4(c4_reg),
+                        .r0(r0_reg),
+                        .r1(r1_reg),
+                        .r2(r2_reg),
+                        .r3(r3_reg),
+                        .h0_new(h0_new),
+                        .h1_new(h1_new),
+                        .h2_new(h2_new),
+                        .h3_new(h3_new),
+                        .h4_new(h4_new)
+                     );
 
 
   //----------------------------------------------------------------
@@ -83,55 +140,65 @@ module poly1305_core(
     begin : reg_update
       if (!reset_n)
         begin
-          s0_reg <= 32'h0;
-          s1_reg <= 32'h0;
-          s2_reg <= 32'h0;
-          s3_reg <= 32'h0;
-          s4_reg <= 32'h0;
+          h0_reg <= 32'h0;
+          h1_reg <= 32'h0;
+          h2_reg <= 32'h0;
+          h3_reg <= 32'h0;
+          h4_reg <= 32'h0;
+
+          c0_reg <= 32'h0;
+          c1_reg <= 32'h0;
+          c2_reg <= 32'h0;
+          c3_reg <= 32'h0;
+          c4_reg <= 32'h0;
+
+          r0_reg <= 32'h0;
+          r1_reg <= 32'h0;
+          r2_reg <= 32'h0;
+          r3_reg <= 32'h0;
+          r4_reg <= 32'h0;
         end
       else
         begin
-          if (s_we)
+          if (h_we)
             begin
-              s0_reg <= s0_new;
-              s1_reg <= s1_new;
-              s2_reg <= s2_new;
-              s3_reg <= s3_new;
-              s4_reg <= s4_new;
+              h0_reg <= h0_new;
+              h1_reg <= h1_new;
+              h2_reg <= h2_new;
+              h3_reg <= h3_new;
+              h4_reg <= h4_new;
+            end
+
+          if (c_we)
+            begin
+              c0_reg <= c0_new;
+              c1_reg <= c1_new;
+              c2_reg <= c2_new;
+              c3_reg <= c3_new;
+              c4_reg <= c4_new;
+            end
+
+          if (r_we)
+            begin
+              r0_reg <= r0_new;
+              r1_reg <= r1_new;
+              r2_reg <= r2_new;
+              r3_reg <= r3_new;
             end
         end
     end // reg_update
 
 
   //----------------------------------------------------------------
-  // poly_block
-  //
-  // Perform acc = (acc + c) * r
+  // poly1305_core_ctrl
   //----------------------------------------------------------------
   always @*
-    begin : poly_block
-      reg [31 : 0] rr0;
-      reg [31 : 0] rr1;
-      reg [31 : 0] rr2;
-      reg [31 : 0] rr3;
+    begin : poly1305_core_ctrl
+      h_we = 1'h0;
+      c_we = 1'h0;
+      r_we = 1'h0;
 
-      s0_new = h0_reg + c0_reg;
-      s1_new = h1_reg + c1_reg;
-      s2_new = h2_reg + c2_reg;
-      s3_new = h3_reg + c3_reg;
-      s4_new = h4_reg + c4_reg;
-
-      rr0 = {2'b0, r0_reg[31 : 2]} * 5;
-      rr1 = {2'b0, r1_reg[31 : 2]} + r1;
-      rr2 = {2'b0, r2_reg[31 : 2]} + r2;
-      rr3 = {2'b0, r3_reg[31 : 2]} + r3;
-
-
-
-
-
-    end // poly_block
-
+    end
 
 endmodule // poly1305_core
 
