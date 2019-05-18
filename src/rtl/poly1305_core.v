632782efec7922ac24d6e49f1ceda2c7d8d61c7e
Joachim Strömbergson <joachim@assured.se>
2019-05-18 19:16:57 +0200

    Adding more functionality to the core. Updated the pblock testbench with info about the test being executed.

--- src/rtl/poly1305_core.v
+++ src/rtl/poly1305_core.v
@@ -51,6 +51,13 @@ module poly1305_core(
                      output wire [127 : 0] mac
                     );
 
+  //----------------------------------------------------------------
+  // Internal constant and parameter definitions.
+  //----------------------------------------------------------------
+  localparam CTRL_IDLE  = 2'h0;
+  localparam CTRL_INIT  = 2'h1;
+  localparam CTRL_NEXT  = 2'h2;
+
 
   //----------------------------------------------------------------
   // Registers including update variables and write enable.
@@ -87,7 +94,23 @@ module poly1305_core(
   reg [31 : 0]  r2_new;
   reg [31 : 0]  r3_reg;
   reg [31 : 0]  r3_new;
+  reg [31 : 0]  r4_reg;
+  reg [31 : 0]  r4_new;
   reg           r_we;
+
+  reg [31 : 0]  pad0_reg;
+  reg [31 : 0]  pad0_new;
+  reg [31 : 0]  pad1_reg;
+  reg [31 : 0]  pad1_new;
+  reg [31 : 0]  pad2_reg;
+  reg [31 : 0]  pad2_new;
+  reg [31 : 0]  pad3_reg;
+  reg [31 : 0]  pad3_new;
+  reg           pad_we;
+
+  reg [1 : 0] poly1305_core_ctrl_reg;
+  reg [1 : 0] poly1305_core_ctrl_new;
+  reg         poly1305_core_ctrl_we;
 
 
   //----------------------------------------------------------------
@@ -95,6 +118,10 @@ module poly1305_core(
   //----------------------------------------------------------------
   reg pblock_next;
   wire pblock_ready;
+
+  reg state_init;
+  reg state_update;
+  reg state_final;
 
 
   //----------------------------------------------------------------
@@ -159,6 +186,13 @@ module poly1305_core(
           r1_reg <= 32'h0;
           r2_reg <= 32'h0;
           r3_reg <= 32'h0;
+
+          pad0_reg <= 32'h0;
+          pad1_reg <= 32'h0;
+          pad2_reg <= 32'h0;
+          pad3_reg <= 32'h0;
+
+          poly1305_core_ctrl_reg <= CTRL_IDLE;
         end
       else
         begin
@@ -187,8 +221,80 @@ module poly1305_core(
               r2_reg <= r2_new;
               r3_reg <= r3_new;
             end
+
+          if (pad_we)
+            begin
+              pad0_reg <= pad0_new;
+              pad1_reg <= pad1_new;
+              pad2_reg <= pad2_new;
+              pad3_reg <= pad3_new;
+            end
+
+          if (poly1305_core_ctrl_we)
+            poly1305_core_ctrl_reg <= poly1305_core_ctrl_new;
         end
     end // reg_update
+
+
+  //----------------------------------------------------------------
+  // poly1305_core_logic
+  //----------------------------------------------------------------
+  always @*
+    begin : poly1305_core_logic
+      h_we   = 1'h0;
+
+      c0_new = 32'h0;
+      c1_new = 32'h0;
+      c2_new = 32'h0;
+      c3_new = 32'h0;
+      c4_new = 32'h0;
+      c_we   = 1'h0;
+
+      r0_new = 32'h0;
+      r1_new = 32'h0;
+      r2_new = 32'h0;
+      r3_new = 32'h0;
+      r4_new = 32'h0;
+      r_we   = 1'h0;
+
+      pad0_new = 32'h0;
+      pad1_new = 32'h0;
+      pad2_new = 32'h0;
+      pad3_new = 32'h0;
+      pad_we   = 1'h0;
+
+      if (state_init)
+        begin
+          c4_new = 32'h1;
+          c_we   = 1'h1;
+
+          r0_new = key[031 : 000];
+          r1_new = key[063 : 032] & 32'hfffffffc;
+          r2_new = key[095 : 064] & 32'hfffffffc;
+          r3_new = key[127 : 096] & 32'hfffffffc;
+          r4_new = key[191 : 128] & 32'hfffffffc;
+          r_we   = 1'h0;
+
+          pad0_new = key[159 : 128];
+          pad1_new = key[191 : 160];
+          pad2_new = key[223 : 192];
+          pad3_new = key[255 : 224];
+          pad_we   = 1'h1;
+        end
+
+      if (state_update)
+        begin
+          c_we = 1'h1;
+          r_we = 1'h1;
+        end
+
+      if (state_final)
+        begin
+          c_we = 1'h1;
+          r_we = 1'h1;
+        end
+
+    end // poly1305_core_logic
 
 
   //----------------------------------------------------------------
@@ -196,10 +302,35 @@ module poly1305_core(
   //----------------------------------------------------------------
   always @*
     begin : poly1305_core_ctrl
-      h_we = 1'h0;
-      c_we = 1'h0;
-      r_we = 1'h0;
+      state_init             = 1'h0;
+      state_update           = 1'h0;
+      state_final            = 1'h0;
+      poly1305_core_ctrl_reg = CTRL_IDLE;
 
+
+      case (poly1305_core_ctrl_reg)
+        CTRL_IDLE:
+          begin
+            if (init)
+              begin
+                state_init = 1'h1;
+              end
+
+            if (next)
+              begin
+                state_update = 1'h1;
+              end
+
+            if (finish)
+              begin
+                state_final = 1'h1;
+              end
+          end
+
+        default:
+          begin
+          end
+      endcase // case (poly1305_core_ctrl_reg)
     end
 
 endmodule // poly1305_core
