e4e22d665ca9f907f02a27f9a07f3dfefedab4c1
Joachim Strömbergson <joachim@assured.se>
2020-08-04 14:55:36 +0200

    Added license header and removed stale build instruction. Changed stale footer.

--- src/model/test_poly1305.c
+++ src/model/test_poly1305.c
@@ -3,8 +3,32 @@
 // -------------
 // A simple experiment to test the Monocypher Poly 1305 implementation.
 //
-// Build with:
-// clang -o experiment -Wall -Wpedantic -Imonocypher.h experiment.c monocypher.c
+// (c) 2020 Joachim Strombergson.
+//
+// Redistribution and use in source and binary forms, with or
+// without modification, are permitted provided that the following
+// conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in
+//    the documentation and/or other materials provided with the
+//    distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+// FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+// COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+// BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+// ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //======================================================================
 
 #include <stdio.h>
@@ -138,5 +162,5 @@ int main(void) {
 }
 
 //======================================================================
-// EOF experiment.c
+// EOF test_poly1305.c
 //======================================================================
